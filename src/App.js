831931df3b64ea91760251ca91c8770b03722eae
HassanShanJava <hassanshan675@gmail.com>
2022-09-24 10:22:29 +0500

    mapping complete

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Portfolio from "./components/Portfolio";
+function App() {
+  return (
+    <div >
+
+      {/* ALWAYS REMEMBER, TAILWINDSCSS IS A MOBILE-FIRST APPROCH*/}
+        <Portfolio/>   
+    </div>
+  );
+}
+
+export default App;
